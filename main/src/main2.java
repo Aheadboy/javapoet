62ab32561b39a3cfa42d60478f2745399e9e2b39
pure <502964379@qq.com>
2019-06-25 21:54:33 +0800

    1、作者注释防盗

---
+++ main/src/main2.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println(        AuthorCopyRight.getCopyRight("linjiejun"));
+
+    }
+}
+
+class AuthorCopyRight {
+
+    public static String getCopyRight(String authorStr) {
+        int MAX = 64;
+        int MIN = 33;
+        Random rand = new Random();
+        char[] authorAuth = new char[authorStr.length() * 2 + 1];
+
+        for (int i = 0, j = 0; i < 1 + authorStr.length() * 2; i++) {
+            if (i % 2 != 0) {
+                authorAuth[i] = authorStr.charAt(j++);
+            } else {
+                authorAuth[i] = (char) (rand.nextInt(MAX - MIN + 1) + MIN);
+            }
+        }
+//        return new String(authorAuth).substring(1, authorAuth.length - 1);
+        return new String(authorAuth);
+    }
+}
